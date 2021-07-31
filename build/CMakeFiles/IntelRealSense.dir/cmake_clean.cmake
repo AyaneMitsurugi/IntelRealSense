e5587f7a7dbd05784d39e4f7fa628e1094b002da
Monika <kmonia197@gmail.com>
2021-07-31 18:16:09 +0200

    [EXAMPLE] Example rs-pose taken from IntelRealSense github page

---
+++ build/CMakeFiles/IntelRealSense.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IntelRealSense.dir/main.cpp.o"
+  "IntelRealSense.pdb"
+  "IntelRealSense"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/IntelRealSense.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
